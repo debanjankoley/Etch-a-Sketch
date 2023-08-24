55529cd550a11b237d0a3d2b8206fa4196a21f83
Debanjan Koley <81432461+debanjankoley@users.noreply.github.com>
2023-08-24 17:36:52 +0530

    Add a feature that lets user decide the grid size
    Clicking the button the user can enter what the number
    of squares can be for x and y axis separately. This is
    useful if someone wants to draw in a larger canvas.

--- main.js
+++ main.js
@@ -1,30 +1,54 @@
 const container = document.querySelector(".container");
 let verticalDivs = [];
 let horizontalDivs = [];
-squaresInYaxis = 16;
-squaresInXaxis = 16;
+let squaresInYaxis = 16;
+let squaresInXaxis = 16;
+createGrid();
 
-// this section creates grid
-for (i = 0; i < squaresInYaxis; i++) {
-    verticalDivs[i] = document.createElement("div");
-    verticalDivs[i].classList.add("rectangle");
-    container.appendChild(verticalDivs[i]);
-    for (j = 0; j < squaresInXaxis; j++) {
-        horizontalDivs[j] = document.createElement("div");
-        horizontalDivs[j].classList.add("square");
-        verticalDivs[i].appendChild(horizontalDivs[j]);
+// this section creates the grid
+function createGrid() {
+    for (i = 0; i < squaresInYaxis; i++) {
+        verticalDivs[i] = document.createElement("div");
+        verticalDivs[i].classList.add("rectangle");
+        container.appendChild(verticalDivs[i]);
+        for (j = 0; j < squaresInXaxis; j++) {
+            horizontalDivs[j] = document.createElement("div");
+            horizontalDivs[j].classList.add("square");
+            verticalDivs[i].appendChild(horizontalDivs[j]);
+        }
     }
 }
-
 // this section colors grid black when hover the div with mouse
-const square = document.querySelectorAll(".square");
-square.forEach(square => square.addEventListener('mouseover', (e) => e.target.classList.add("color")))
+let square = document.querySelectorAll(".square");
+square.forEach(square => square.addEventListener('mouseover', 
+    (e) => e.target.classList.add("color")));
+
 
 // this section clears the grid when reset button is clicked
 const resetButton = document.querySelector(".reset");
-function reset(e) {
+function toReset(e) {
     square.forEach(square => square.classList.remove("color"));
 };
-resetButton.addEventListener("click", reset);
+resetButton.addEventListener("click", toReset);
 
 // this section changes the no. of squares in the grid based on user input
+const userInput = document.querySelector(".user-input")
+function gettingInput(e) {
+    square.forEach(square => square.classList.remove("color"));
+    inputXaxis = Number(prompt("Enter the no. of squares for X-axis :", 16));
+    inputYaxis = Number(prompt("Enter the no. of squares for Y-axis :", 16));
+    if (isNaN(inputXaxis && inputYaxis) || inputXaxis && inputYaxis <= 0) {
+        alert("Not a valid number");
+    } else {
+        while (container.firstChild) {
+            container.removeChild(container.lastChild);
+        };
+        squaresInXaxis = inputXaxis;
+        squaresInYaxis = inputYaxis;
+        createGrid();
+        square = document.querySelectorAll(".square");
+        square.forEach(square => square.addEventListener('mouseover', 
+            (e) => e.target.classList.add("color")));
+    }
+}
+userInput.addEventListener("click", gettingInput);
